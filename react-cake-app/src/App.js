c5ff7f9942e5d860fee4811094df134b1d1aa36d
jie_chen <jie.chen@outlook.com.au>
2023-06-23 01:25:04 +0930

    install react-router-dom and set up Route

--- react-cake-app/src/App.js
+++ react-cake-app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- react-cake-app/src/components/Navbar.js
+++ react-cake-app/src/components/Navbar.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Logo from '../assets/cakeLogo.png'
+import React from 'react';
+import Logo from '../assets/cakeLogo.png';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
 	return (
@@ -7,7 +8,12 @@ function Navbar() {
 			<div className='leftSide'>
 				<img src={Logo} alt="" />
 			</div>
-			<div className='rightSide'></div>
+			<div className='rightSide'>
+				<Link to='/'> Home </Link>
+				<Link to='/menu'> Menu </Link>
+				<Link to='/about'> About </Link>
+				<Link to='/contact'> Contact </Link>
+			</div>
 		</div>
 	)
 }
