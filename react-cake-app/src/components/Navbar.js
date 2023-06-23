64535933e8b30496bac55274357075ff9c5911e3
jie_chen <jie.chen@outlook.com.au>
2023-06-23 12:24:15 +0930

    install material icons "ReorderIcon "and make links in navbar disapear once screen shrinks less than 600px

--- react-cake-app/src/components/Navbar.js
+++ react-cake-app/src/components/Navbar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Logo from '../assets/cakeLogo.png';
 import {Link} from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css'
 
 function Navbar() {
 	return (
 		<div className='navbar'>
 			<div className='leftSide'>
-				<img src={Logo} alt="" />
+				<img src={Logo} alt="specialty-cakes-logo" />
+
 			</div>
 			<div className='rightSide'>
 				<Link to='/'> Home </Link>
 				<Link to='/menu'> Menu </Link>
 				<Link to='/about'> About </Link>
 				<Link to='/contact'> Contact </Link>
+				<button>
+					<ReorderIcon />
+				</button>
+				
 			</div>
 		</div>
 	)
