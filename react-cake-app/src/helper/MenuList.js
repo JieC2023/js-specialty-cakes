892e6cf5576297f32cdb46b229e655cd623e9ac4
jie_chen <jie.chen@outlook.com.au>
2023-06-24 20:36:55 +0930

    set up menu page and its style

---
+++ react-cake-app/src/helper/MenuList.js
@@ -0,0 +1,39 @@
+import cheeseCake from "../assets/cheeseCake.jpg";
+import specialtyCake from "../assets/specialtyCake.jpg";
+import swissRoll from "../assets/swissRoll.jpg";
+import stawberryCake from "../assets/stawberryCake.jpeg";
+import chocoCake from "../assets/chocoCake.jpeg";
+import spongeCake from "../assets/spongeCake.jpg";
+
+export const MenuList = [
+  {
+    name: "Cheese Cake",
+    image: cheeseCake,
+    price: 14.99,
+  },
+  {
+    name: "Specialty Cake",
+    image: specialtyCake,
+    price: 20.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: swissRoll,
+    price: 16.59,
+  },
+  {
+    name: "Vegan Pizza",
+    image: stawberryCake,
+    price: 19.49,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: chocoCake,
+    price: 12.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: spongeCake,
+    price: 13.99,
+  },
+];
