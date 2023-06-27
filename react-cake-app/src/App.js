682a22facff44684ec6d39542b503c59c8dd31f3
jie_chen <jie.chen@outlook.com.au>
2023-06-27 15:02:57 +0930

    add in about page

--- react-cake-app/src/App.js
+++ react-cake-app/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import { Cart } from './pages/Cart';
+import About from './pages/About';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' exact element={<Home />} />
           <Route path='/menu' exact element={<Menu />} />
           <Route path='/cart' exact element={<Cart />} />
+          <Route path='/about' exact element={<About />} />
         </Routes>
         <Footer />
       </Router>
